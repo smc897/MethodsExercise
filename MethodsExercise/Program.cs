d27db1b9bbfad49dd550aecaa948e0a4463f46d5
ScottMC89 <scottmc592@gmail.com>
2023-02-17 11:11:06 -0600

    story stuff.

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            story();
+        }
+
+        //function to tell a story
+        static void story() {
+            Console.WriteLine("enter a name:");
+            String name = Console.ReadLine();
+            Console.WriteLine("enter a verb:");
+            String verb = Console.ReadLine();
+            Console.WriteLine("enter a toy:");
+            String toy = Console.ReadLine();
+            Console.WriteLine(name + "" + verb + " with " + toy + " today. ");
         }
     }
 }
